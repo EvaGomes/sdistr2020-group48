4cff5887ba725e594b58f51a5d376a37e16d6b36
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-05 09:40:47 +0100

    Add stub for source/data.c

---
+++ source/data.c
@@ -0,0 +1,27 @@
+/* Grupo 48
+ *   Eva Gomes (37806)
+ *   João Santos (40335)
+ *   João Vieira (45677)
+ */
+
+#include "data.h"
+
+struct data_t* data_create(int size) {
+	return 0; // TODO
+}
+
+struct data_t* data_create2(int size, void* data) {
+	return 0; // TODO
+}
+
+void data_destroy(struct data_t* data) {
+	// TODO
+}
+
+struct data_t* data_dup(struct data_t* data) {
+	return 0; // TODO
+}
+
+void data_replace(struct data_t* data, int new_size, void* new_data) {
+	// TODO
+}
