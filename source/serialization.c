4b368bd9a26cfc2b3f5fa35524c688da91f9ad74
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-06 18:03:34 +0100

    Implement data_to_buffer and buffer_to_data

--- source/serialization.c
+++ source/serialization.c
@@ -7,13 +7,50 @@
 #include "data.h"
 #include "entry.h"
 #include "tree.h"
+#include <stdlib.h>
+#include <string.h>
 
-int data_to_buffer(struct data_t* data, char** data_buf) {
-  return 0; // TODO
+const int SIZE_OF_DATASIZE = sizeof(int);
+
+int data_to_buffer(struct data_t* dataStruct, char** data_buf) {
+  if (dataStruct == NULL || data_buf == NULL) {
+    return -1;
+  }
+
+  int* pointer_datasize = &(dataStruct->datasize);
+  void* pointer_data = dataStruct->data;
+
+  int len_datasize = SIZE_OF_DATASIZE;
+  int len_data = dataStruct->datasize;
+
+  int index_datasize = 0;
+  int index_data = len_datasize;
+
+  int len_buffer = len_datasize + len_data;
+  char* buffer = malloc(len_buffer);
+  memcpy(buffer + index_datasize, pointer_datasize, len_datasize);
+  memcpy(buffer + index_data, pointer_data, len_data);
+
+  *data_buf = buffer;
+  return len_buffer;
 }
 
-struct data_t* buffer_to_data(char* data_buf, int data_buf_size) {
-  return 0; // TODO
+struct data_t* buffer_to_data(char* buffer, int buffer_size) {
+  if (buffer == NULL || buffer_size <= 0) {
+    return NULL;
+  }
+  
+  int datasize;
+  int len_datasize = SIZE_OF_DATASIZE;
+  int index_datasize = 0;
+  memcpy(&datasize, buffer + index_datasize, len_datasize);
+
+  void* data = malloc(datasize);
+  int len_data = datasize;
+  int index_data = index_datasize + len_datasize;
+  memcpy(data, buffer + index_data, len_data);
+
+  return data_create2(datasize, data);
 }
 
 int entry_to_buffer(struct entry_t* entry, char** entry_buf) {
