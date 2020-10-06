0f0f6e6dad2a3f7c300f9d17ec054d2654e0aa90
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-06 15:32:23 +0100

    Add stub for source/serialization.c

---
+++ source/serialization.c
@@ -0,0 +1,33 @@
+/* Grupo 48
+ *   Eva Gomes (37806)
+ *   João Santos (40335)
+ *   João Vieira (45677)
+ */
+
+#include "data.h"
+#include "entry.h"
+#include "tree.h"
+
+int data_to_buffer(struct data_t* data, char** data_buf) {
+  return 0; // TODO
+}
+
+struct data_t* buffer_to_data(char* data_buf, int data_buf_size) {
+  return 0; // TODO
+}
+
+int entry_to_buffer(struct entry_t* entry, char** entry_buf) {
+  return 0; // TODO
+}
+
+struct entry_t* buffer_to_entry(char* entry_buf, int entry_buf_size) {
+  return 0; // TODO
+}
+
+int tree_to_buffer(struct tree_t* tree, char** tree_buf) {
+  return 0; // TODO
+}
+
+struct entry_t* buffer_to_tree(char* tree_buf, int tree_buf_size) {
+  return 0; // TODO
+}
