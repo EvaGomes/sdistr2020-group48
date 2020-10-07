4bf8093fadb2c4fbab21c424f496521e99a9e7b5
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-07 15:33:36 +0100

    Add stub for source/tree.c

---
+++ source/tree.c
@@ -0,0 +1,45 @@
+/* Grupo 48
+ *   Eva Gomes (37806)
+ *   João Santos (40335)
+ *   João Vieira (45677)
+ */
+
+#include "data.h"
+#include "tree-private.h"
+#include "tree.h"
+
+struct tree_t* tree_create() {
+    return 0; //TODO
+}
+
+void tree_destroy(struct tree_t* tree) {
+    //TODO
+}
+
+int tree_put(struct tree_t* tree, char* key, struct data_t* value) {
+    return 0; //TODO
+}
+
+struct data_t* tree_get(struct tree_t* tree, char* key) {
+    return 0; //TODO
+}
+
+int tree_del(struct tree_t* tree, char* key) {
+    return 0; //TODO
+}
+
+int tree_size(struct tree_t* tree) {
+    return 0; //TODO
+}
+
+int tree_height(struct tree_t* tree) {
+    return 0; //TODO
+}
+
+char** tree_get_keys(struct tree_t* tree) {
+    return 0; //TODO
+}
+
+void tree_free_keys(char** keys) {
+    //TODO
+}
