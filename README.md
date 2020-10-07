b5c7c6acbe9e70deedb84e53c7a7703f00641f31
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-07 11:54:16 +0100

    Implement entry_to_buffer and buffer_to_entry

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sdistr2020-group48-proj1
+
+
+## Known limitations:
+- Serialization of entries does not handle entries with null keys or null values.
--- source/serialization.c
+++ source/serialization.c
@@ -39,7 +39,7 @@ struct data_t* buffer_to_data(char* buffer, int buffer_size) {
   if (buffer == NULL || buffer_size <= 0) {
     return NULL;
   }
-  
+
   int datasize;
   int len_datasize = SIZE_OF_DATASIZE;
   int index_datasize = 0;
@@ -54,11 +54,52 @@ struct data_t* buffer_to_data(char* buffer, int buffer_size) {
 }
 
 int entry_to_buffer(struct entry_t* entry, char** entry_buf) {
-  return 0; // TODO
+  if (entry == NULL || entry->key == NULL || entry->value == NULL || entry_buf == NULL) {
+    return -1;
+  }
+
+  char** pointer_value = malloc(sizeof(char*));
+
+  int len_len_key = sizeof(int);
+  int len_key = strlen(entry->key) + 1;
+  int len_value = data_to_buffer(entry->value, pointer_value);
+
+  int index_len_key = 0;
+  int index_key = len_len_key;
+  int index_value = len_len_key + len_key;
+
+  int buffer_size = len_len_key + len_key + len_value;
+  char* buffer = malloc(buffer_size);
+  memcpy(buffer + index_len_key, &len_key, len_len_key);
+  memcpy(buffer + index_key, entry->key, len_key);
+  memcpy(buffer + index_value, *pointer_value, len_value);
+
+  free(*pointer_value);
+  free(pointer_value);
+
+  *entry_buf = buffer;
+  return buffer_size;
 }
 
-struct entry_t* buffer_to_entry(char* entry_buf, int entry_buf_size) {
-  return 0; // TODO
+struct entry_t* buffer_to_entry(char* buffer, int buffer_size) {
+  if (buffer == NULL || buffer_size <= 0) {
+    return NULL;
+  }
+
+  int len_key;
+  int len_len_key = sizeof(len_key);
+  int index_len_key = 0;
+  memcpy(&len_key, buffer + index_len_key, len_len_key);
+
+  char* key = malloc(len_key);
+  int index_key = index_len_key + len_len_key;
+  memcpy(key, buffer + index_key, len_key);
+
+  int index_value = index_key + len_key;
+  int len_value = buffer_size - index_value;
+  struct data_t* value = buffer_to_data(buffer + index_value, len_value);
+
+  return entry_create(key, value);
 }
 
 int tree_to_buffer(struct tree_t* tree, char** tree_buf) {
