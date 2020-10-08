e1622c66ff14bc221ee77947be9d6d5aedc1b5e8
EvaGomes <EvaGomes@users.noreply.github.com>
2020-10-08 11:51:21 +0100

    Implement tree_put

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 
 ## Known limitations:
+- Tree does not balance itself when inserting new nodes.
 - Serialization of entries does not handle entries with null keys or null values.
--- source/tree.c
+++ source/tree.c
@@ -40,8 +40,70 @@ void tree_destroy(struct tree_t* tree) {
   }
 }
 
+/* Creates a node for the given key and value (with no children). */
+struct tree_node_t* _node_create(char* key, struct data_t* value) {
+  struct tree_node_t* newNode = malloc(SIZE_OF_TREE_NODE_T);
+  newNode->entry = entry_create(key, value);
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
+}
+
+/* Returns the maximum between two ints. */
+int _int_max(int n1, int n2) {
+  return n1 > n2 ? n1 : n2;
+}
+
+/* Core iterative algorithm of tree_put.
+ *  Expects the tree to have a root, and the key and value to be ready to store.
+ */
+void _tree_put(struct tree_t* tree, char* copied_key, struct data_t* copied_value) {
+  struct tree_node_t* current_tree_node = tree->root;
+  int current_depth = 0;
+
+  while (1) {
+    int comparison = key_compare(copied_key, current_tree_node->entry->key);
+
+    if (comparison == 0) {
+      entry_destroy(current_tree_node->entry);
+      current_tree_node->entry = entry_create(copied_key, copied_value);
+      return; // replaced current_tree_node's entry (no changes in size nor height)
+    }
+
+    struct tree_node_t** pointer_to_next_node =
+        (comparison < 0 ? &(current_tree_node->left) : &(current_tree_node->right));
+
+    if (*pointer_to_next_node == NULL) {
+      *pointer_to_next_node = _node_create(copied_key, copied_value);
+      tree->size += 1;
+      tree->height = _int_max(current_depth + 1, tree->height);
+      return; // inserted new node below the current_tree_node
+    }
+
+    // update vars for next iteration of the walk
+    current_tree_node = *pointer_to_next_node;
+    current_depth += 1;
+  }
+}
+
 int tree_put(struct tree_t* tree, char* key, struct data_t* value) {
-  return 0; // TODO
+  if (tree == NULL) {
+    return -1;
+  }
+
+  char* copied_key = key == NULL ? NULL : strdup(key);
+  struct data_t* copied_value = data_dup(value);
+
+  if (tree->root == NULL) {
+    // insert root node
+    tree->root = _node_create(copied_key, copied_value);
+    tree->size = 1;
+    tree->height = 0;
+  } else {
+    // insert node in the correct place
+    _tree_put(tree, copied_key, copied_value);
+  }
+  return 0;
 }
 
 struct data_t* tree_get(struct tree_t* tree, char* key) {
