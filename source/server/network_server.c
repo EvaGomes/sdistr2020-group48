7994f18709fa6d6781f79700c0c047ed08751887
EvaGomes <EvaGomes@users.noreply.github.com>
2020-11-01 18:40:36 +0000

    Support several requests from each client

--- source/server/network_server.c
+++ source/server/network_server.c
@@ -47,33 +47,22 @@ int network_server_init(short port) {
   return listening_socket;
 }
 
-int network_main_loop(int listening_socket) {
-
-  int connsockfd;
-  struct sockaddr_in client;
+int _client_connection_loop(int connsockfd) {
   char str[MAX_MSG + 1];
   int nbytes, count;
-  socklen_t size_client = 0;
 
-  // accept bloqueia à espera de pedidos de conexão.
-  // Quando retorna já foi feito o "three-way handshake" e connsockfd é uma
-  // socket pronta a comunicar com o cliente.
-  while ((connsockfd = accept(listening_socket, (struct sockaddr*) &client, &size_client)) != -1) {
-
-    printf("connsockfd=%d - Accepted client connection\n", connsockfd);
+  while (1) {
 
     // Lê string (no máximo MAX_MSG bytes) enviada pelo cliente
     // através da socket
     if ((nbytes = read(connsockfd, str, MAX_MSG)) < 0) {
       fprintf(stderr, "connsockfd=%d - Error while reading request-data from client\n", connsockfd);
-      close(connsockfd);
-      continue;
+      return -1;
     }
-
-    printf("connsockfd=%d - Received request-data from client\n", connsockfd);
 
     // Coloca terminador de string
     str[nbytes] = '\0';
+    printf("connsockfd=%d - Received request-data from client\n    %s\n", connsockfd, str);
     // Conta número de carateres
     count = strlen(str);
     // Converte count para formato de rede
@@ -83,12 +72,28 @@ int network_main_loop(int listening_socket) {
     if ((nbytes = write(connsockfd, &count, sizeof(count))) != sizeof(count)) {
       fprintf(stderr, "connsockfd=%d - Error while sending response-data to the client\n",
               connsockfd);
-      close(connsockfd);
-      continue;
+      return -1;
     }
 
-    printf("connsockfd=%d - Sent response-data to client\n", connsockfd);
+    printf("connsockfd=%d - Sent response-data to client\n    %d\n", connsockfd, count);
+  }
+}
+
+int network_main_loop(int listening_socket) {
+
+  int connsockfd;
+  struct sockaddr_in client;
+  socklen_t size_client = 0;
+
+  // accept bloqueia à espera de pedidos de conexão.
+  // Quando retorna já foi feito o "three-way handshake" e connsockfd é uma
+  // socket pronta a comunicar com o cliente.
+  while ((connsockfd = accept(listening_socket, (struct sockaddr*) &client, &size_client)) != -1) {
+
+    printf("connsockfd=%d - Accepted client connection\n", connsockfd);
+    _client_connection_loop(connsockfd);
     close(connsockfd);
+    printf("connsockfd=%d - Closed client connection\n", connsockfd);
   }
   return 0; // TODO
 }
