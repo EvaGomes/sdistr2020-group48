adde01450bc34dac19fb11e30586dd91914840d8
EvaGomes <EvaGomes@users.noreply.github.com>
2020-11-01 19:44:14 +0000

    Create aliases for the entries of the enums in Message

--- include/message-private.h
+++ include/message-private.h
@@ -16,6 +16,24 @@ struct message_t {
   Message* msg;
 };
 
+/* Aliases to entries of Message__OperationCode* */
+#define OP_BAD MESSAGE__OPERATION_CODE__OP_BAD
+#define OP_SIZE MESSAGE__OPERATION_CODE__OP_SIZE
+#define OP_DEL MESSAGE__OPERATION_CODE__OP_DEL
+#define OP_GET MESSAGE__OPERATION_CODE__OP_GET
+#define OP_PUT MESSAGE__OPERATION_CODE__OP_PUT
+#define OP_GETKEYS MESSAGE__OPERATION_CODE__OP_GETKEYS
+#define OP_HEIGHT MESSAGE__OPERATION_CODE__OP_HEIGHT
+#define OP_ERROR MESSAGE__OPERATION_CODE__OP_ERROR
+
+/* Aliases to entries of Message__ContentCase* */
+#define CT_NONE MESSAGE__CONTENT__NOT_SET
+#define CT_KEY MESSAGE__CONTENT_KEY
+#define CT_VALUE MESSAGE__CONTENT_VALUE
+#define CT_ENTRY MESSAGE__CONTENT_ENTRY
+#define CT_KEYS MESSAGE__CONTENT_KEYS
+#define CT_INT_RESULT MESSAGE__CONTENT_INT_RESULT
+
 DataMessage* data_to_msg(struct data_t* data);
 struct data_t* msg_to_data(DataMessage* sg);
 
