abfdade4f50421a1d69054578aee450bab69e823
EvaGomes <EvaGomes@users.noreply.github.com>
2020-11-10 12:01:56 +0000

    Fix tree_client

--- source/client/tree_client.c
+++ source/client/tree_client.c
@@ -9,9 +9,12 @@
 #include "network_client.h"
 
 #include <errno.h>
-#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char* collect_input() {
+/* Collects a string, with maximum length MAX_MSG, written by the user in the console. */
+char* _collect_input() {
 
   char* input_str = malloc(MAX_MSG * sizeof(char));
   printf("> ");
@@ -25,6 +28,93 @@ char* collect_input() {
   free(input_str);
 
   return str;
+}
+
+/* Creates an entry_t from the given key and string-data. */
+struct entry_t* _entry_create_from_args(char* key, char* data) {
+  if (key == NULL || data == NULL) {
+    return NULL;
+  }
+  struct data_t* dataStruct = data_create2(strlen(data) + 1, strdup(data));
+  return (dataStruct == NULL) ? NULL : entry_create(strdup(key), dataStruct);
+}
+
+/* Prints the given keys to the console. */
+void _print_keys(char** keys) {
+  printf("< keys: [");
+  int index = 0;
+  while (keys[index] != NULL) {
+    printf("\n    %s", keys[index]);
+    index += 1;
+  }
+  if (index == 0) {
+    printf("] (#=0)\n");
+  } else {
+    printf("\n  ] (#=%d)\n", index);
+  }
+}
+
+void _run_command(struct rtree_t* rtree, char* command, char* key, char* data) {
+
+  if (strcmp(command, "size") == 0) {
+    int size = rtree_size(rtree);
+    printf("< size: %d\n", size);
+  }
+
+  else if (strcmp(command, "height") == 0) {
+    int height = rtree_height(rtree);
+    printf("< height: %d\n", height);
+  }
+
+  else if (strcmp(command, "del") == 0) {
+    if (key == NULL) {
+      printf("< Invalid args. Usage: del <key>\n");
+    } else if (rtree_del(rtree, key) < 0) {
+      printf("< Operation failed!\n");
+    } else {
+      printf("< Done!\n");
+    }
+  }
+
+  else if (strcmp(command, "get") == 0) {
+    if (key == NULL) {
+      printf("< Invalid args. Usage: get <key>\n");
+    } else {
+      struct data_t* value = rtree_get(rtree, key);
+      if (value == NULL) {
+        printf("< Query failed!\n");
+      } else if (value->data == NULL) {
+        printf("< Key not found!\n");
+        data_destroy(value);
+      } else {
+        printf("< value: %s\n", (char*) value->data);
+      }
+    }
+  }
+
+  else if (strcmp(command, "put") == 0) {
+    struct entry_t* entry = _entry_create_from_args(key, data);
+    if (entry == NULL) {
+      printf("< Invalid args. Usage: put <key> <data>\n");
+    } else if (rtree_put(rtree, entry) < 0) {
+      printf("< Operation failed!\n");
+    } else {
+      printf("< Done!\n");
+    }
+  }
+
+  else if (strcmp(command, "getkeys") == 0) {
+    char** keys = rtree_get_keys(rtree);
+    if (keys == NULL) {
+      printf("< Query failed!\n");
+    } else {
+      _print_keys(keys);
+    }
+  }
+
+  else {
+    printf("< Unknown command!\n");
+  }
 }
 
 int main(int argc, char** argv) {
@@ -44,52 +134,28 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  int sockfd = rtree->sockfd;
-
   while (1) {
 
-    char* input_str = collect_input();
-    int input_str_len = strlen(input_str);
+    char* input_str = _collect_input();
+    if (input_str == NULL) {
+      printf("< Failed to read the command. Please, try again.");
+      continue;
+    }
 
     if (strcmp(input_str, "quit") == 0) {
       free(input_str);
       break;
     }
 
-    char* command = input_str; // TODO: parse input string
-    char *key, data;
+    char* delimiter = " ";
+    char* rest = NULL;
+    char* command = strtok_r(input_str, delimiter, &rest);
+    char* key = strtok_r(rest, delimiter, &rest);
+    char* data = (strlen(rest) == 0) ? NULL : rest;
 
-    if (strcmp(command, "put") == 0) {
-      printf("// TODO parse input_str and get 'key' and 'data'\n");
-    } else if (strcmp(command, "get") == 0) {
-      printf("// TODO parse input_str and get 'key'\n");
-      // char* value = rtree_get(rtree, key);
-      // if (value == NULL) {
-      //   printf("Failed!");
-      // } else {
-      //   printf("value: %s", value);
-      // }
-    } else if (strcmp(command, "del") == 0) {
-      printf("// TODO parse input_str and get 'key'\n");
-      // if (rtree_del(rtree, key) < 0) {
-      //   printf("Failed!");
-      // } else {
-      //   printf("Done!");
-      // }
-    } else if (strcmp(input_str, "size") == 0) {
-      printf("size: %d\n", rtree_size(rtree));
-    } else if (strcmp(input_str, "height") == 0) {
-      printf("height: %d\n", rtree_height(rtree));
-    } else if (strcmp(input_str, "getkeys") == 0) {
-      char** keys = rtree_get_keys(rtree);
-      if (keys == NULL) {
-        printf("Failed!\n");
-      } else {
-        printf("keys: // TODO print keys\n");
-      }
-    } else {
-      printf("unknown command!\n");
-    }
+    _run_command(rtree, command, key, data);
+
+    free(input_str);
   }
 
   printf("Quitting...");
