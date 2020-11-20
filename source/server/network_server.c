3b618d160156ecb35c3a5a60bc7d6e1b8db4c1f9
EvaGomes <EvaGomes@users.noreply.github.com>
2020-11-20 01:26:36 +0000

    Support multiple clients

--- source/server/network_server.c
+++ source/server/network_server.c
@@ -10,8 +10,15 @@
 #include "tree_skel.h"
 
 #include <netinet/in.h>
+#include <poll.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define SIZE_OF_SOCKADDR sizeof(struct sockaddr)
+
+/* The number of clients supported simultaneously by the server. */
+#define MAX_CLIENT_SOCKETS 5
 
 int network_server_init(short port) {
   int listening_socket;
@@ -61,40 +68,102 @@ int network_send(int client_socket, struct message_t* msg) {
   return network_send_message(client_socket, msg);
 }
 
-int _client_connection_loop(int connsockfd) {
-  while (1) {
-
-    struct message_t* message = network_receive(connsockfd);
-    if (message == NULL) {
-      return -1;
+/* Closes all client sockets (does not close the listening_socket). */
+void _close_all_client_sockets(struct pollfd* sockets) {
+  for (int i = 1; i <= MAX_CLIENT_SOCKETS; i++) {
+    if (sockets[i].fd != -1) {
+      close(sockets[i].fd);
     }
+  }
+}
 
-    logger_info__sockfd(connsockfd, "Handling message from client...");
-    if (invoke(message) < 0) {
-      message_destroy(message);
-      return -1;
+/* Handles the events of the listening socket.
+ *  Expects the first position of the given array of sockets to be the listening_socket.
+ */
+void _handle_listening_socket_events(struct pollfd* sockets, int* client_sockets_count) {
+  struct pollfd listening_socket = sockets[0];
+  if ((listening_socket.revents & POLLIN) && (*client_sockets_count < MAX_CLIENT_SOCKETS)) {
+    // find first available index
+    int i = 1;
+    while (sockets[i].fd != -1) {
+      i += 1;
     }
+    // try to accept new connection and set it at index i
+    struct sockaddr client;
+    socklen_t size_client = SIZE_OF_SOCKADDR;
+    sockets[i].fd = accept(listening_socket.fd, &client, &size_client);
+    if (sockets[i].fd > 0) {
+      sockets[i].events = POLLIN;
+      sockets[i].revents = 0;
+      *client_sockets_count += 1;
+      logger_info__sockfd(sockets[i].fd, "Accepted client connection");
+    } else {
+      logger_error("_handle_listening_socket_events", "Failed to accept client connection");
+    }
+  }
+}
 
-    int send_result = network_send(connsockfd, message);
+/* Reads the message from the given client socket and sends the response back to it.
+ *   Returns 0 if the message was handled and responded correctly, or -1 if an error occurred.
+ */
+int _handle_client_socket_pollin_event(int client_socket) {
+  struct message_t* message = network_receive(client_socket);
+  if (message == NULL) {
+    return -1;
+  }
+  logger_info__sockfd(client_socket, "Handling message from client...");
+  if (invoke(message) < 0) {
     message_destroy(message);
-    if (send_result < 0) {
-      return -1;
+    return -1;
+  }
+  int send_result = network_send(client_socket, message);
+  message_destroy(message);
+
+  return (send_result < 0) ? -1 : 0;
+}
+
+/* Handles the events in the given client sockets. */
+void _handle_client_sockets_events(struct pollfd* sockets, int* client_sockets_count) {
+  for (int i = 1; i <= *client_sockets_count; ++i) {
+    struct pollfd client_socket = sockets[i];
+    if (client_socket.revents & POLLIN) {
+      int handler_result = _handle_client_socket_pollin_event(client_socket.fd);
+      if (handler_result < 0) {
+        close(client_socket.fd);
+        logger_info__sockfd(client_socket.fd, "Closed client connection");
+        client_socket.fd = -1;
+        *client_sockets_count -= 1;
+      }
     }
   }
 }
 
 int network_main_loop(int listening_socket) {
-
-  int connsockfd;
-  struct sockaddr_in client;
-  socklen_t size_client = 0;
-
-  while ((connsockfd = accept(listening_socket, (struct sockaddr*) &client, &size_client)) != -1) {
-    logger_info__sockfd(connsockfd, "Accepted client connection");
-    _client_connection_loop(connsockfd);
-    close(connsockfd);
-    logger_info__sockfd(connsockfd, "Closed client connection");
+  struct pollfd sockets[1 + MAX_CLIENT_SOCKETS];
+  sockets[0].fd = listening_socket;
+  sockets[0].events = POLLIN;
+  for (int i = 1; i <= MAX_CLIENT_SOCKETS; i++) {
+    sockets[i].fd = -1;
   }
+  int client_sockets_count = 0;
+
+  while (1) {
+    int num_of_sockets_with_events = poll(sockets, 1 + client_sockets_count, -1);
+
+    if (num_of_sockets_with_events < 0) {
+      logger_error("network_main_loop", "Poll failed");
+      _close_all_client_sockets(sockets);
+      return -1;
+    } else if (num_of_sockets_with_events == 0) {
+      // this is not expected to happen because we are not setting a timeout.
+      // simply log it and proceed
+      logger_error("network_main_loop", "Unexpected timeout");
+    } else {
+      _handle_listening_socket_events(sockets, &client_sockets_count);
+      _handle_client_sockets_events(sockets, &client_sockets_count);
+    }
+  }
+
   return 0;
 }
 
